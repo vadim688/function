4c5fd49f56eb859678536de8758303cc80f85b2f
Vadim Bulgaru <vadimbulgaru9@gmail.com>
2024-10-22 16:30:16 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function isEven(number) {
+    return number % 2 === 0;
+  }
+  
+  console.log(isEven(4)); 
+console.log(isEven(7)); 
+
+function findMax(arr) {
+    return Math.max(...arr);
+  }
+  
+  console.log(isEven(4)); 
+console.log(isEven(7)); 
+
+console.log(findMax([1, 5, 3, 9, 2])); // 9
